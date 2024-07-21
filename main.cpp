f8d66f30f172953cc037d7c51f24f55ec6c9a869
Benjamin Lehmann <bclehmann@yahoo.ca>
2024-07-20 23:30:38 -0600

    trying stuff, want to go to sleep :/

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 #include <cstdio>
+#include <cstdint>
 #include <llvm-c/Core.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
@@ -13,10 +14,62 @@
 #include <llvm/ADT/ArrayRef.h>
 #include <llvm/IR/InstrTypes.h>
 
-struct LLVMValueDeleter {
-	void operator()(llvm::Value* p) {
-		p->deleteValue();
+enum class AcceptDecision : int32_t {
+	Consume, // Not a match, but can try again with a later substring
+	Accept, // Match
+	Reject // Not a match
+};
+
+class Atom {
+public:
+	virtual llvm::Function* codegen() = 0;
+};
+
+class Literal : public Atom {
+public:
+	Literal(char to_match, llvm::LLVMContext* context, llvm::Module* module, llvm::IRBuilder<>* builder)
+	: to_match{to_match}, context{context}, module{module}, builder{builder}
+	{}
+	llvm::Function* codegen() override {
+		llvm::Function* existing = module->getFunction(get_generated_function_name());
+
+		if (existing) {
+			return existing;
+		}
+
+		llvm::FunctionType* generated_function_type = llvm::FunctionType::get(llvm::Type::getInt32Ty(*context), false);
+		llvm::Function* generated_function = llvm::Function::Create(generated_function_type, llvm::Function::PrivateLinkage, get_generated_function_name());
+
+		llvm::FunctionType* getchar_type = llvm::FunctionType::get(llvm::Type::getInt32Ty(*context), false);
+		llvm::Function* getchar_function = llvm::Function::Create(getchar_type, llvm::Function::ExternalLinkage, "getchar", module);
+
+		llvm::BasicBlock* entry = llvm::BasicBlock::Create(*context, "entry", generated_function);
+		builder->SetInsertPoint(entry);
+		llvm::Value* input = builder->CreateCall(getchar_type, getchar_function);
+		llvm::Value* is_equal = builder->CreateICmpEQ(input, llvm::ConstantInt::get(llvm::Type::getInt32Ty(*context), to_match));
+
+		llvm::BasicBlock* matches = llvm::BasicBlock::Create(*context, "matches", generated_function);
+		builder->SetInsertPoint(matches);
+		builder->CreateRet(llvm::ConstantInt::get(llvm::Type::getInt32Ty(*context), static_cast<int32_t>(AcceptDecision::Accept), true));
+
+		llvm::BasicBlock* does_not_match = llvm::BasicBlock::Create(*context, "does_not_match", generated_function);
+		builder->SetInsertPoint(does_not_match);
+		builder->CreateRet(llvm::ConstantInt::get(llvm::Type::getInt32Ty(*context), static_cast<int32_t>(AcceptDecision::Consume), true));
+
+		builder->SetInsertPoint(entry);
+		builder->CreateCondBr(is_equal, matches, does_not_match);
+
+		return generated_function;
 	}
+private:
+	std::string get_generated_function_name() const {
+		return module->getModuleIdentifier() + "literal_matching__" + to_match;
+	}
+
+	char to_match;
+	llvm::LLVMContext* context;
+	llvm::Module* module;
+	llvm::IRBuilder<>* builder;
 };
 
 int main(int argc, char* argv[]) {
@@ -25,11 +78,17 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	std::string regex = argv[1];
 
 	llvm::LLVMContext context;
 	llvm::IRBuilder Builder(context);
 	llvm::Module module("RegexCompiler", context);
+
+	std::string regex = argv[1];
+	std::vector<Atom*> atoms;
+	for(char c : regex) {
+		Literal literal(c, &context, &module, &Builder);
+		atoms.push_back(&literal);
+	}
 
 	std::vector<llvm::Type*> getchar_args_type;
 	llvm::FunctionType* getchar_type = llvm::FunctionType::get(llvm::Type::getInt32Ty(context), getchar_args_type, false);
@@ -46,6 +105,32 @@ int main(int argc, char* argv[]) {
 
 	llvm::BasicBlock* entry = llvm::BasicBlock::Create(context, "entry", main_function);
 	Builder.SetInsertPoint(entry);
+
+	llvm::BasicBlock* pre_init_loop = llvm::BasicBlock::Create(context, "pre_init_loop", main_function);
+	Builder.CreateBr(pre_init_loop);
+	Builder.SetInsertPoint(pre_init_loop);
+
+	llvm::AllocaInst* buf = Builder.CreateAlloca(llvm::Type::getInt8Ty(context), llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), 1024), "buf");
+	llvm::Value* start_index = llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), 0);
+
+	llvm::BasicBlock* init_array_loop = llvm::BasicBlock::Create(context, "init_array_loop", main_function);
+	llvm::BasicBlock* after_init_loop = llvm::BasicBlock::Create(context, "after_init_loop", main_function);
+	Builder.CreateBr(init_array_loop);
+
+	Builder.SetInsertPoint(init_array_loop);
+
+	llvm::PHINode* loop_index = Builder.CreatePHI(llvm::Type::getInt32Ty(context), 2);
+	llvm::Value* next_index = Builder.CreateAdd(loop_index, llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), 1));
+	loop_index->addIncoming(start_index, pre_init_loop);
+	loop_index->addIncoming(next_index, init_array_loop);
+	Builder.CreateStore(
+		Builder.CreateIntCast(Builder.CreateAdd(loop_index, llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), 'A')), llvm::Type::getInt8Ty(context), true),
+		Builder.CreateGEP(llvm::Type::getInt8Ty(context), buf, std::vector<llvm::Value*> { loop_index })
+	);
+	Builder.CreateCondBr(Builder.CreateICmpEQ(loop_index, llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), 10)), after_init_loop, init_array_loop);
+
+	Builder.SetInsertPoint(after_init_loop);
+	Builder.CreateCall(puts_type, puts_function, buf);
 
 	std::vector<llvm::Value*> loop_values;
 	std::vector<llvm::BasicBlock*> loop_blocks;
